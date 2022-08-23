218dea2cce59614ce6fd3ad2ea0ea5cd10cc372c
Dilmurod Djuraev <dilmurod.djuraev86@gmail.com>
2022-08-23 08:31:27 -0700

    addded byeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,10 @@
+package first;
+
+public class ByeWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Maybe 5 weeks of Java left");
+        System.out.println("1,5 weeks of git");
+    }
+}
